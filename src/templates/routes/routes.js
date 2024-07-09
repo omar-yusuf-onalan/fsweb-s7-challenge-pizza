f71eaefa2a122bd7e455fd19fae9649718c6292d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-09 18:54:53 +0300

    Create routes.js

---
+++ src/templates/routes/routes.js
@@ -0,0 +1,11 @@
+import homeRoute from "./homeRoute.js";
+import orderPizzaRoute from "./orderPizzaRoute.js";
+import successRoute from "./successRoute.js";
+
+const routes = [
+    homeRoute,
+    orderPizzaRoute,
+    successRoute
+]
+
+export default routes
