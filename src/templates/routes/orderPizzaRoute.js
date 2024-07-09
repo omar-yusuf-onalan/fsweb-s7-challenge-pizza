02b5d956a50c94fde0b956a211a1713ed9b073ab
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 00:51:14 +0300

    Add information about route

--- src/templates/routes/orderPizzaRoute.js
+++ src/templates/routes/orderPizzaRoute.js
@@ -1,8 +1,34 @@
 import OrderPizza from "../../pages/OrderPizza/OrderPizza.jsx";
 
 const orderPizzaRoute = {
-    path: '/order-pizza',
-    element: OrderPizza
+    path: "/order-pizza",
+    element: OrderPizza,
+    priceForSize: {
+        kucuk: 20,
+        orta: 40,
+        buyuk: 60
+    },
+    priceForThickness: {
+        ince: 20,
+        kalin: 40
+    },
+    maxNumberOfToppings: 10,
+    priceForEachTopping: 5,
+    additionalToppings: [
+        "Pepperoni",
+        "Sosis",
+        "Kanada Jambonu",
+        "Tavuk Izgara",
+        "Soğan",
+        "Domates",
+        "Mısır",
+        "Sucuk",
+        "Jalepeno",
+        "Sarımsak",
+        "Biber",
+        "Ananas",
+        "Kabak"
+    ]
 }
 
 export default orderPizzaRoute;
