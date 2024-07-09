0b48ac5f9a97012612287a7302286fe40dd14f62
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 00:33:03 +0300

    Add three components and create price state

--- src/pages/OrderPizza/OrderPizza.jsx
+++ src/pages/OrderPizza/OrderPizza.jsx
@@ -1,10 +1,18 @@
 import styles from './OrderPizza.module.css';
-import React from "react";
+import React, {useState} from "react";
+import Header from "../../components/Header/Header.jsx";
+import PizzaBuilder from "../../components/PizzaBuilder/PizzaBuilder.jsx";
+import TotalPrice from "../../components/TotalPrice/TotalPrice.jsx";
 
 const OrderPizza = () => {
+
+    const [price, setPrice] = useState(0);
+
     return (
         <div>
-            order pizza
+            <Header />
+            <PizzaBuilder setPrice={setPrice} />
+            <TotalPrice price={price} />
         </div>
     )
 }
