d3fb3f0a13f3cadf6770e60fa92867613159b537
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-09 18:54:29 +0300

    Create OrderPizza.jsx

---
+++ src/pages/OrderPizza/OrderPizza.jsx
@@ -0,0 +1,11 @@
+import styles from './OrderPizza.module.css';
+
+const OrderPizza = () => {
+    return (
+        <div>
+            order pizza
+        </div>
+    )
+}
+
+export default OrderPizza;
