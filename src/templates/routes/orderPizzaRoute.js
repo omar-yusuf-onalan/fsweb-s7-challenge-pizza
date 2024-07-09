23516211e5a48cd60442958b82416b583049082a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-09 18:54:48 +0300

    Create orderPizzaRoute.js

---
+++ src/templates/routes/orderPizzaRoute.js
@@ -0,0 +1,8 @@
+import OrderPizza from "../../pages/OrderPizza/OrderPizza.jsx";
+
+const orderPizzaRoute = {
+    path: '/order-pizza',
+    element: OrderPizza
+}
+
+export default orderPizzaRoute;
