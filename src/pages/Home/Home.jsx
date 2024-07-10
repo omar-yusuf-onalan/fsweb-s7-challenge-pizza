e362632d752c23d8a291b758da20993dec3ed3b6
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 11:01:35 +0300

    Add background and button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import styles from './Home.module.css';
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            home
+        <div className={styles.home}>
+            <img src="../../../Assets/mile1-assets/logo.svg" alt="logo"/>
+
+            <div className={`${styles.caption} ${styles.captionFonts}`}>
+                <div className={styles.alignText}>
+                    <p>KOD ACIKTIRIR</p>
+                    <p>PIZZA, DOYURUR</p>
+                </div>
+
+                <button className={styles.yellowButton} onClick={() => navigate("/order-pizza")}>ACIKTIM</button>
+            </div>
         </div>
     )
-}
 
+}
 export default Home;
