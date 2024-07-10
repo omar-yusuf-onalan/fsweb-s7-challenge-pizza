9a81037d021ce416dcf3c1021c40b113d942cef5
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 10:52:08 +0300

    Add price calculating functionality

--- src/components/TotalPrice/TotalPrice.jsx
+++ src/components/TotalPrice/TotalPrice.jsx
@@ -1,9 +1,36 @@
 import styles from './TotalPrice.module.css';
 
 const TotalPrice = ({pizza, route}) => {
+    const {pricesForSize, pricesForThickness, priceForEachTopping} = route;
+
+    const calculatePrice = () => {
+        const basePrice = pizza.singularPrice;
+        const priceOfToppings = pizza.malzemeler.length * priceForEachTopping;
+        const sizePrice = pricesForSize[pizza.boyut] || 0;
+        const thicknessPrice = pricesForThickness[pizza.hamur] || 0;
+
+        return pizza.adet * (basePrice + priceOfToppings + sizePrice + thicknessPrice);
+    }
+
     return (
         <div>
+            <div>
+                <button>-</button>
+                {pizza.adet}
+                <button>+</button>
+            </div>
 
+            <div>
+
+                <div>
+                    <p>Sipariş Toplamı</p>
+                    <p>Seçimler <span></span></p>
+                    <p>Toplam {calculatePrice()}</p>
+                </div>
+
+                <button>SİPARİŞ VER</button>
+
+            </div>
         </div>
     )
 }
