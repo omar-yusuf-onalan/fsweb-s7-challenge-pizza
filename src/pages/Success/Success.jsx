6b2e810da8c60dfe21ee1cce0b2072f0bac16adb
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 11:59:51 +0300

    Update Success.jsx

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,10 +1,17 @@
 import styles from './Success.module.css';
 import React from "react";
+import Logo from "../../components/Logo/Logo.jsx";
 
 const Success = () => {
     return (
-        <div>
-            success
+        <div className={styles.success}>
+            <Logo position="absolute" />
+
+            <div className={styles.caption}>
+                <p>TEBRIKLER!</p>
+                <p>SİPARİŞİNİZ ALINDI!</p>
+            </div>
+
         </div>
     )
 }
