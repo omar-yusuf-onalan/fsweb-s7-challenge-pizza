60d71e364a311eea8cb8f72d8a733616262088db
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 15:47:09 +0300

    Create Description.jsx

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,30 @@
+import styles from "./Description.module.css";
+
+const Description = () => {
+    return (
+        <div>
+            <p>Position Absolute Acı Pizza</p>
+
+            <div>
+                <span>85.50₺</span>
+
+                <div>
+                    <span>4.9</span>
+
+                    <span>(200)</span>
+                </div>
+            </div>
+
+            <div>
+                <p>
+                    Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre.
+                    Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel
+                    yuvarlak, düzleştirilmiş mayalı buğday bazlı
+                    hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Description;
