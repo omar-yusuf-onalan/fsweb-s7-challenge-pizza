9594e6c3ca3cb0fd770b9011777cecc75eddcac6
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 12:00:26 +0300

    Add prop to select the position attribute of image

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,13 @@
 import styles from "./Logo.module.css";
 import React from "react";
 
-const Logo = () => {
+const Logo = ({position = "static"}) => {
+    const logoStyle = {
+        position: position
+    }
+
     return (
-        <img className={styles.image} src="../../../Assets/mile1-assets/logo.svg" alt="logo"/>
+        <img style={logoStyle} className={styles.image} src="../../../Assets/mile1-assets/logo.svg" alt="logo"/>
     )
 }
 
