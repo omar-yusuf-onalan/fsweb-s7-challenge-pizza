42d0ad3093a5f6362d65566704f38badc3f172df
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 15:47:23 +0300

    Replace descriptions with Description component

--- src/components/PizzaBuilder/PizzaBuilder.jsx
+++ src/components/PizzaBuilder/PizzaBuilder.jsx
@@ -1,5 +1,6 @@
 import styles from './PizzaBuilder.module.css';
 import {Form, FormGroup, Input, Label} from "reactstrap";
+import Description from "../Description/Description.jsx";
 
 const PizzaBuilder = ({setPizza, route}) => {
 
@@ -38,25 +39,7 @@ const PizzaBuilder = ({setPizza, route}) => {
 
     return (
         <div>
-            <p>Position Absolute Acı Pizza</p>
-
-            <div>
-                <span>85.50₺</span>
-
-                <div>
-                    <span>4.9</span>
-
-                    <span>(200)</span>
-                </div>
-            </div>
-
-            <div>
-                <p>
-                    Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre.
-                    Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel yuvarlak, düzleştirilmiş mayalı buğday bazlı
-                    hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
-                </p>
-            </div>
+            <Description />
 
             <div>
                 <div>
