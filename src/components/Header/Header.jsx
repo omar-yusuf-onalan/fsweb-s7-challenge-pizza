2257d4ea46075733237fc601425d3bc58a9c0093
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-10 11:22:51 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import styles from './Header.module.css';
+import Logo from "../Logo/Logo.jsx";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
+            <div>
+                <Logo />
 
+                <div>
+                    <Link to="/">Anasayfa</Link>
+                    -
+                    <Link>Seçenekler</Link>
+                    -
+                    <Link to="order-pizza">Sipariş Oluştur</Link>
+                </div>
+            </div>
         </div>
     )
 }
