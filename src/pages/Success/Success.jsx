6afa997b319fa00e3b497b5b9dbc0c75f529103e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-07-11 13:59:11 +0300

    Add pizza details to success page

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,16 +1,39 @@
 import styles from './Success.module.css';
 import React from "react";
 import Logo from "../../components/Logo/Logo.jsx";
+import orderPizzaRoute from "../../templates/routes/orderPizzaRoute.js";
+import PizzaPrice from "../../components/PizzaPrice/PizzaPrice.jsx";
 
-const Success = () => {
+const Success = ({pizza}) => {
     return (
         <div className={styles.success}>
-            <Logo position="absolute" />
+            <Logo/>
 
             <div className={styles.caption}>
-                <p>TEBRIKLER!</p>
                 <p><span>SİPARİŞİNİZ</span> <span>ALINDI!</span></p>
             </div>
+
+            <div className={styles.whiteLine}></div>
+
+            <div className={styles.orderDetails}>
+
+
+
+                    <p>Position Absolute Acı Pizza</p>
+
+                    <div className={styles.pizzaDetails}>
+                        <p>Boyut: {pizza.boyut}</p>
+                        <p>Hamur: {pizza.hamur}</p>
+                        <p>Ek Malzemeler: {pizza.malzemeler.join(", ")}</p>
+                    </div>
+
+                    <div>
+                        <PizzaPrice pizza={pizza} priceForEachTopping={orderPizzaRoute.priceForEachTopping}/>
+                    </div>
+
+
+            </div>
+
 
         </div>
     )
